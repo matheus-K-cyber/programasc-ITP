16808f406696432ca2254e97e0aa5bf8b87ff3ae
Matheus Kauã Neri da Silva <matheus.994555879@gmail.com>
2026-04-09 16:42:21 -0300

    Tentativa 1.3, adições de mais variaveis como contadores e vetores para medias, preparação dos printf's.

--- alunosnotas.c
+++ alunosnotas.c
@@ -1,34 +1,57 @@
 #include <stdio.h>
 
 int main() {
-    int alunos, chamada, medias;
+    int alunos, chamada;
+    float media;
     int aprovados[100] = {0}, recuperacao[100] = {0}, reprovados[100] = {0};
+    int aprove = 0, rere = 0, reprove = 0;
+    float notap[100] = {0.0}, notar[100] = {0.0}, notaf[100] = {0.0};
+    float np = 0.0, nr = 0.0, nf = 0.0;
     int i,j, k, l;
 
     scanf("%d", &alunos);
 
     for(i = 0; i < alunos; i++) {
-        scanf("%d - %d", &chamada, &medias);
+        scanf("%d %f", &chamada, &media);
 
-        if(medias >= 7) {
-            aprovados[alunos] = chamada;
-            alunos++;
+        if(media >= 7.0) {
+            aprovados[aprove] = chamada;
+            aprove++;
+            notap[np] = media;
+            np++;
 
-        } else if(medias >= 5 && medias < 7) {
-            recuperacao[alunos] = chamada;
-            alunos++;
+        } else if(media >= 5.0 && media < 7.0) {
+            recuperacao[rere] = chamada;
+            rere++;
+            notar[nr] = media;
+            nr++;
 
-        } else if(medias < 5) {
-            reprovados[alunos] = chamada;
-            alunos++;
+        } else if(media < 5.0) {
+            reprovados[reprove] = chamada;
+            reprove++;
+            notaf[nf] = media;
+            nf++;
+
         }
     }
 
-    for(j = 0; j < 100; j++) {
-        scanf("%d", &aprovados[j]);
+    for(j = 0; j < aprove; j++) {
+        scanf("%d %f", &aprovados[aprove], notap[np]);
+
+        printf("Aprovados: %d (%f)\n", aprove, np);
     }
 
-    printf("Aprovados: %d\n", aprovados[j]);
+    for(k = 0; k < rere; k++) {
+        scanf("%d %f", &recuperacao[rere], notar[nr]);
+
+        printf("Recuperação: %d (%f)\n", rere, nr);
+    }
+
+    for(l = 0; l < reprove; l++) {
+        scanf("%d %f", &reprovados[reprove], &notaf[nf]);
+
+        printf("Reprovados: %d (%f)\n", reprove, nf);
+    }
 
     return 0;
 }
