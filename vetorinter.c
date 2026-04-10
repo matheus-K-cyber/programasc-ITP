7fe127fdfddaa2cb9f1cb00d93ef4efb41fed345
Matheus Kauã Neri da Silva <matheus.994555879@gmail.com>
2026-04-10 20:32:07 -0300

    Tentativa 1.5, correção na main e no funcionamento da função intercalar.

--- vetorinter.c
+++ vetorinter.c
@@ -1,55 +1,51 @@
 #include <stdio.h>
 
-int intercalar(int v1[], int v2[], int v3[], int n1, int n2) {
+int intercalar(int v1[], int v2[], int v3[], int tam1, int tam2) {
     int i = 0, j = 0, k = 0;
 
-    while(i < n1 && j < n2) {
-        if(v1[i] < v2[j]) {
-            v3[k] = v1[i];
-            i++;
-        } else {
-            v3[k] = v2[j];
-            j++;
-        }
-        k++;
-    }
-
-    while(i < n1) {
+    while(i < tam1 && j < tam2) {
         v3[k] = v1[i];
+        k++;
         i++;
-        k++;
-    }
-
-    while(j < n2) {
         v3[k] = v2[j];
-        j++;
         k++;
+        j++;
     }
 
-    return k;
+    if(i < tam1) {
+        v3[k] = v1[i];
+        k++;
+        i++;
+    }
+
+    if(j < tam2) {
+        v3[k] = v2[j];
+        k++;
+        j++;
+    }
+
+return k;
 }
 
 int main() {
     int tam1, tam2, n1 = 0, n2 = 0, n3 = 0;
-    int v1[10] = {0}, v2[10] = {0}, v3[10] = {0};
+    int v1[10] = {0}, v2[10] = {0}, v3[20] = {0};
 
     scanf("%d", &tam1);
 
-    for(int i = 0; i < tam1 - 1; i++) {
+    for(int i = 0; i < tam1; i++) {
         scanf("%d", &n1);
         v1[i] = n1;
-        i++;
     }
 
     scanf("%d", &tam2);
 
-    for(int i = 0; i < tam2 - 1; i++) {
+    for(int i = 0; i < tam2; i++) {
         scanf("%d", &n2);
         v2[i] = n2;
-        i++;
     }
 
-    n3 = intercalar(v1, v2, v3, n1, n2);
+    n3 = intercalar(v1, v2, v3, tam1, tam2);
 
     printf("Resultado: ");
     for(int i = 0; i < n3; i++) {
