99dc9725899531cfe403e6f1852063c05415ef40
Matheus Kauã Neri da Silva <matheus.994555879@gmail.com>
2026-04-10 21:18:02 -0300

    Tentativa 1.4, correção nos for r printf's, remoção de n.

--- separacaodiv.c
+++ separacaodiv.c
@@ -3,48 +3,46 @@
 int main() {
     int vetorA[15] = {0}, vetorB[15] = {0}, vetorC[15] = {0}, vetorD[15] = {0};
     int tamA = 0, tamB = 0, tamC = 0, tamD = 0;
-    int n, i;
+    int i;
 
-    scanf("%d", &tamA);
-
-    for(i = 0; i < tamA; i++) {
-        scanf("%d", &n);
-        vetorA[i] = n;
+    for(i = 0; i < 15; i++) {
+        scanf("%d", &tamA);
+        vetorA[tamA] = i;
+        tamA++;
     }
 
-    if(tamA == 15) {
-        for(i = 0; i < tamA; i++) {
+    for(i = 0; i < 15; i++) {
             if(vetorA[i] % 2 == 0) {
                 vetorB[tamB] = vetorA[i];
                 tamB++;
-            } else if(vetorA[i] % 3 == 0) {
+            } 
+            if(vetorA[i] % 3 == 0) {
                 vetorC[tamC] = vetorA[i];
                 tamC++;
-            } else {
+            } 
+            if(vetorA[i] % 2 != 0 && vetorA[i] % 3 != 0) {
                 vetorD[tamD] = vetorA[i];
                 tamD++;
             }
         }
-
-    }
     
-    printf("Vetor B: ");
+    printf("Vetor B: [");
     for(i = 0; i < tamB; i++) {
         printf("%d ", vetorB[i]);
     }
-    printf("\n");
+    printf("]\n");
 
-    printf("Vetor C: ");
+    printf("Vetor C: [");
     for(i = 0; i < tamC; i++) {
         printf("%d ", vetorC[i]);
     }
-    printf("\n");
+    printf("]\n");
 
-    printf("Vetor D: ");
+    printf("Vetor D: [");
     for(i = 0; i < tamD; i++) {
         printf("%d ", vetorD[i]);
     }
-    printf("\n");
+    printf("]\n");
 
     return 0;
 }
