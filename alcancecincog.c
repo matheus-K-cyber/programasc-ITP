b8ef716af2d6512fdca3e04c6e1d4f85a7dc84e4
matheus-K-cyber <matheus.994555879@gmail.com>
2026-04-28 17:24:12 -0300

    Código para alcance 5G e determinar células próximas do usuário, caso ele esteja conectado/dentro do alcance(completo).

--- alcancecincog.c
+++ alcancecincog.c
@@ -2,33 +2,27 @@
 #include <stdlib.h>
 
 int get_range(int n, int cells[n], int distance, int user, int *left_index, int *right_index) {
-    int i;
+    int i, count = 0;
     int user_rangeL = user - distance, user_rangeR = user + distance;
 
     *left_index = -1;
     *right_index = -1;
 
     for (i = 0; i < n; i++) {
-        if (cells[i] >= user_rangeL) {
+        if (cells[i] >= user_rangeL && cells[i] <= user_rangeR) {
             if (*left_index == -1) {
                 *left_index = i;
             }
-        } else if (cells[i] <= user_rangeR) {
-            if (*right_index == -1) {
-                *right_index = i;
-            }
-        } else {
-            break;
+            *right_index = i;
+            count++;
         }
     }
 
-    
-
-    return 0;
+    return count;
 }
 
 int main() {
-    int n, distance, user, i, left_index, right_index;
+    int n, distance, user, i, left_index, right_index, total = 0;
 
     scanf("%d %d %d", &n, &distance, &user);
 
@@ -38,7 +32,16 @@ int main() {
         scanf("%d", &cells[i]);
     }
 
-    get_range(n, cells, distance, user, &left_index, &right_index);
+    total = get_range(n, cells, distance, user, &left_index, &right_index);
+
+    if (total == 0) {
+        printf("USUARIO DESCONECTADO\n");
+    } else {
+        for (i = left_index; i <= right_index; i++) {
+            printf("%d ", cells[i]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
